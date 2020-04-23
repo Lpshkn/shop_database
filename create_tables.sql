5a186431879dfd4344ca38d243b496a010f00a5f
lpshkn <kofffsh@gmail.com>
2020-04-24 00:20:28 +0300

    Arrange blocks, add integrity constraints, remove unnecessary tables, add new table
    This alignment is unnecessary and was decided to change it. Remove Users and Addresses tables because they are unnecessary. Table of discount cards was added.

--- create_tables.sql
+++ create_tables.sql
@@ -1,169 +1,57 @@
--- CREATE DATABASE shop_db;
+USE shop_db
 
-CREATE TABLE users -- пользователи системы
+CREATE TABLE products
 (
-user_id      INT 
-             PRIMARY KEY
-             NOT NULL,
- 
-username     VARCHAR(64)
-             UNIQUE
-             NOT NULL,
+product_id INT IDENTITY PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
 
-pass_hash    VARCHAR(128)
-             NOT NULL
-);
-
-CREATE TABLE products  
-(
-product_id   INT 
-             PRIMARY KEY
-             NOT NULL,
- 
-name         VARCHAR(64)
-             UNIQUE
-             NOT NULL
+CONSTRAINT unique_name UNIQUE(name)
 );
 
 CREATE TABLE workers
 (
-worker_id    INT 
-             PRIMARY KEY
-             NOT NULL,
-
-user_id      INT
-             REFERENCES users
-             UNIQUE
-             NOT NULL,
-		
-first_name   VARCHAR(16)
-             NOT NULL,
-
-second_name  VARCHAR(16)
-             NOT NULL,
-		
-salary       INT,
-		
-position     VARCHAR(32)
-);
-
-CREATE TABLE addresses
-(
-address_id   INT 
-             PRIMARY KEY
-             NOT NULL,
-
-city         VARCHAR(32)
-             NOT NULL,
-
-country      VARCHAR(32)
-             NOT NULL,
-		 
-postal_code  VARCHAR(16)
-             UNIQUE
-             NOT NULL
+worker_id INT IDENTITY PRIMARY KEY,
+first_name VARCHAR(32) NOT NULL,
+second_name VARCHAR(32) NOT NULL,
+salary MONEY,
+job VARCHAR(32)
 );
 
 CREATE TABLE customers
 (
-customer_id  INT 
-             PRIMARY KEY
-             NOT NULL,
-
-user_id      INT
-             REFERENCES users
-             UNIQUE
-             NOT NULL,
-
-address_id   INT
-             REFERENCES addresses,
-		 
-first_name   VARCHAR(16) 
-             NOT NULL,
-		 
-second_name  VARCHAR(16) 
-             NOT NULL
+customer_id INT IDENTITY PRIMARY KEY,
+first_name VARCHAR(32) NOT NULL,
+second_name VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE suppliers
 (
-supplier_id  INT 
-             PRIMARY KEY
-             NOT NULL,
-
-user_id      INT
-             REFERENCES users
-             NOT NULL,
-		 
-product_id   INT
-             REFERENCES products
-             NOT NULL,
-
-address_id   INT
-             REFERENCES addresses,
-		 
-name         VARCHAR(128)
-             NOT NULL
+supplier_id INT IDENTITY PRIMARY KEY,
+product_id INT NOT NULL,
+name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE warehouses
 (
-warehouse_id INT
-             PRIMARY KEY
-             NOT NULL,
-   
-product_id   INT
-             REFERENCES products
-             UNIQUE
-             NOT NULL,
-		 
-quantity     INT
-             NOT NULL
+warehouse_id INT IDENTITY PRIMARY KEY,
+product_id INT UNIQUE NOT NULL,
+quantity INT NOT NULL
 );
 
-CREATE TABLE orders -- покупка товара клиентом
+CREATE TABLE orders
 (
-order_id     INT
-             PRIMARY KEY
-             NOT NULL,
-
-product_id   INT
-             REFERENCES products
-             NOT NULL,
-			
-customer_id  INT
-             REFERENCES customers
-             NOT NULL,
-			
-quantity     INT
-             NOT NULL,
-		   
-cost         INT
-             NOT NULL,
-
-date         TIMESTAMP
+order_id INT IDENTITY PRIMARY KEY,
+product_id INT NOT NULL,
+customer_id INT NOT NULL,
+quantity SMALLINT NOT NULL,
+cost MONEY NOT NULL,
+date DATETIME
 );
 
-CREATE TABLE deliveries -- заказ товара у поставщика
+CREATE TABLE discount_cards
 (
-delivery_id  INT
-             PRIMARY KEY
-             NOT NULL,
-
-product_id   INT
-             REFERENCES products
-             NOT NULL,
-			
-supplier_id  INT
-             REFERENCES suppliers
-             NOT NULL,
-			
-quantity     INT
-             NOT NULL,
-		   
-cost         INT
-             NOT NULL,
-
-date         TIMESTAMP
+card_id INT IDENTITY PRIMARY KEY,
+discount TINYINT NOT NULL DEFAULT 0,
+start_date DATE NOT NULL DEFAULT GETDATE(),
+expiration DATE NOT NULL DEFAULT DATEADD(day, 7, start_date)
 );
-
