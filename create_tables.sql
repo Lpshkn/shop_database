17986de3ccacba8c9dbcc1b51831c343d1b578e6
lpshkn <kofffsh@gmail.com>
2020-04-24 01:52:51 +0300

    Delete unnecessary table, arrange all tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,63 +1,87 @@
 USE shop_db
 
-CREATE TABLE products
+CREATE TABLE discount_cards
 (
-product_id INT IDENTITY PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
-
-CONSTRAINT unique_name UNIQUE(name)
-);
-
-CREATE TABLE workers
-(
-worker_id INT IDENTITY PRIMARY KEY,
-fullname VARCHAR(128) NOT NULL,
-salary MONEY,
-job VARCHAR(32)
+card_id INT IDENTITY PRIMARY KEY,
+discount TINYINT NOT NULL DEFAULT 0,
+start_date DATE NOT NULL DEFAULT GETDATE(),
+expiration DATE NOT NULL
 );
 
 CREATE TABLE customers
 (
 customer_id INT IDENTITY PRIMARY KEY,
-first_name VARCHAR(32) NOT NULL,
-second_name VARCHAR(32) NOT NULL
+fullname VARCHAR(128) NOT NULL,
+card_id INT,
+
+CONSTRAINT card_foreign FOREIGN KEY (card_id)
+    REFERENCES discount_cards(card_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE suppliers
 (
 supplier_id INT IDENTITY PRIMARY KEY,
-product_id INT NOT NULL,
-name VARCHAR(255) NOT NULL
+name VARCHAR(255) NOT NULL UNIQUE,
+address VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE warehouses
+CREATE TABLE products
 (
-warehouse_id INT IDENTITY PRIMARY KEY,
-product_id INT UNIQUE NOT NULL,
-quantity INT NOT NULL
+product_id INT IDENTITY PRIMARY KEY,
+name VARCHAR(255) NOT NULL UNIQUE,
+quantity_stock SMALLINT NOT NULL,
+supplier_name VARCHAR(255) NOT NULL,
+
+CONSTRAINT supplier_foreign FOREIGN KEY (supplier_name)
+    REFERENCES suppliers(name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE purchases
 (
-purchase_id INT NOT NULL PRIMARY KEY,
-product_id INT NOT NULL PRIMARY KEY,
+purchase_id INT NOT NULL UNIQUE,
+product_id INT NOT NULL,
 quantity SMALLINT NOT NULL,
-default_cost MONEY NOT NULL
+default_cost MONEY NOT NULL,
+
+CONSTRAINT purchases_prim PRIMARY KEY (purchase_id, product_id),
+CONSTRAINT product_foreign FOREIGN KEY (product_id)
+    REFERENCES products(product_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 )
+
+CREATE TABLE workers
+(
+worker_id INT IDENTITY PRIMARY KEY,
+fullname VARCHAR(128) NOT NULL UNIQUE,
+salary MONEY,
+job VARCHAR(32)
+);
 
 CREATE TABLE receipts
 (
 receipt_id INT IDENTITY PRIMARY KEY,
 customer_id INT NOT NULL,
 purchase_id INT NOT NULL,
-worker_name VARCHAR(128) NOT NULL,
-date DATETIME
-);
+worker_name VARCHAR(128),
+date DATETIME,
 
-CREATE TABLE discount_cards
-(
-card_id INT IDENTITY PRIMARY KEY,
-discount TINYINT NOT NULL DEFAULT 0,
-start_date DATE NOT NULL DEFAULT GETDATE(),
-expiration DATE NOT NULL DEFAULT DATEADD(day, 7, start_date)
+CONSTRAINT fullname_foreign FOREIGN KEY (worker_name)
+    REFERENCES workers(fullname)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+
+CONSTRAINT customer_foreign FOREIGN KEY (customer_id)
+    REFERENCES customers(customer_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+CONSTRAINT purchase_foreign FOREIGN KEY (purchase_id)
+    REFERENCES purchases(purchase_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
