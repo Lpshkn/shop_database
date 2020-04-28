26b1d4faaa802ffea849a70d32b2768a51dad81f
lpshkn <kofffsh@gmail.com>
2020-04-29 00:45:47 +0300

    Add main function and MainWindow class to process the main window
    This class implements the .ui interface of the same name

---
+++ desktop/main_window.py
@@ -0,0 +1,23 @@
+"""
+This module represents the class to work with the main window.
+"""
+from os.path import join, dirname
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    # Define the directory of ui's files
+    UI_DIR = join(dirname(__file__), 'resources', 'ui')
+    # Define the filename to ui file of that widget
+    FILENAME_UI = join(UI_DIR, 'main_window.ui')
+
+    def __init__(self):
+        super().__init__()
+
+        # Load .ui file and initialize it
+        try:
+            uic.loadUi(self.FILENAME_UI, self)
+        except FileNotFoundError as e:
+            print(e)
+            exit(-1)
