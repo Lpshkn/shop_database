64ef6b4841be76b0d297186a43236be44ff55004
lpshkn <kofffsh@gmail.com>
2020-05-09 21:29:27 +0300

    Add description to the connection method, add additional parameter into the signature of the method

--- desktop/database.py
+++ desktop/database.py
@@ -6,12 +6,20 @@ class Database:
         self.connection = None
 
     def connect_db(self, server: str, database: str, name: str, password: str, autocommit: bool = False,
-                   timeout: int = 4) -> pyodbc.Connection:
+                   timeout: int = 4, driver: str = "DRIVER={ODBC Driver 17 for SQL Server}") -> pyodbc.Connection:
         """
-        This function connects to database and returns connection object
+        This method connects to the database and returns the connection object.
+
+        :param server: the name of the server to connect
+        :param database: the name of the database contained on the server and which you want to connect
+        :param name: the name of the user which will be connected to the database
+        :param password: the password of the user
+        :param autocommit: the flag which specifies that each query will be committed immediately
+        :param timeout: time what the server will wait while connecting
+        :param driver: the driver of the ODBC to connect to the database using this driver
         """
 
-        self.connection = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}", server=server, database=database,
+        self.connection = pyodbc.connect(driver, server=server, database=database,
                                          uid=name, pwd=password, autocommit=autocommit, timeout=timeout)
 
         return self.connection
