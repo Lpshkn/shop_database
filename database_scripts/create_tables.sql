89c77600741632b35b35db309b076ca695e8870e
lpshkn <kofffsh@gmail.com>
2020-08-13 20:25:00 +0300

    Edit the creating tables script of the SQL Server
    For now the tables will create if they don't exist

--- database_scripts/create_tables.sql
+++ database_scripts/create_tables.sql
@@ -1,189 +1,210 @@
 USE shopdb
 
-CREATE TABLE DiscountCards
-(
-card_id      INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'DiscountCards')
+    BEGIN
+        CREATE TABLE DiscountCards
+        (
+        card_id      INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-discount     FLOAT
-             NOT NULL
-             DEFAULT 0,
+        discount     FLOAT
+                     NOT NULL
+                     DEFAULT 0,
 
-start_date   DATE
-             NOT NULL
-             DEFAULT GETDATE(),
+        start_date   DATE
+                     NOT NULL
+                     DEFAULT GETDATE(),
 
-expiration   DATE
-             NOT NULL
-);
+        expiration   DATE
+                     NOT NULL
+        );
+    END
 
-CREATE TABLE Customers
-(
-customer_id  INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+    BEGIN
+        CREATE TABLE Customers
+        (
+        customer_id  INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-fullname     NVARCHAR(128)
-             NOT NULL,
+        fullname     NVARCHAR(128)
+                     NOT NULL,
 
-card_id      INT,
+        card_id      INT,
 
-address      VARCHAR(128)
-             NOT NULL,
+        address      VARCHAR(128)
+                     NOT NULL,
 
-email        VARCHAR(128)
-             UNIQUE,
+        email        VARCHAR(128)
+                     UNIQUE,
 
-telephone    VARCHAR(16)
-             UNIQUE,
+        telephone    VARCHAR(16)
+                     UNIQUE,
 
-CONSTRAINT card_foreign FOREIGN KEY (card_id)
-    REFERENCES DiscountCards(card_id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
+        CONSTRAINT card_foreign FOREIGN KEY (card_id)
+            REFERENCES DiscountCards(card_id)
+                ON DELETE SET NULL
+                ON UPDATE CASCADE
+        );
+    END
 
-CREATE TABLE Suppliers
-(
-supplier_id  INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Suppliers')
+    BEGIN
+        CREATE TABLE Suppliers
+        (
+        supplier_id  INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-name         NVARCHAR(128)
-             NOT NULL
-             UNIQUE,
+        name         NVARCHAR(128)
+                     NOT NULL
+                     UNIQUE,
 
-address      NVARCHAR(128)
-             NOT NULL,
+        address      NVARCHAR(128)
+                     NOT NULL,
 
-email        VARCHAR(128)
-             UNIQUE,
+        email        VARCHAR(128)
+                     UNIQUE,
 
-telephone    VARCHAR(16)
-             UNIQUE
-);
+        telephone    VARCHAR(16)
+                     UNIQUE
+        );
+    END
 
-CREATE TABLE Producers
-(
-producer_id  INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Producers')
+    BEGIN
+        CREATE TABLE Producers
+        (
+        producer_id  INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-name         NVARCHAR(128)
-             NOT NULL
-             UNIQUE,
+        name         NVARCHAR(128)
+                     NOT NULL
+                     UNIQUE,
 
-address      NVARCHAR(128)
-             NOT NULL,
+        address      NVARCHAR(128)
+                     NOT NULL,
 
-email        VARCHAR(128)
-             UNIQUE,
+        email        VARCHAR(128)
+                     UNIQUE,
 
-telephone    VARCHAR(16)
-             UNIQUE
-)
+        telephone    VARCHAR(16)
+                     UNIQUE
+        )
+    END
 
-CREATE TABLE Products
-(
-product_id   INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Products')
+    BEGIN
+        CREATE TABLE Products
+        (
+        product_id   INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-name         NVARCHAR(128)
-             NOT NULL
-             UNIQUE,
+        name         NVARCHAR(128)
+                     NOT NULL
+                     UNIQUE,
 
-producer     NVARCHAR(128)
-             NOT NULL,
+        producer     NVARCHAR(128)
+                     NOT NULL,
 
-quantity     SMALLINT
-             NOT NULL,
+        quantity     SMALLINT
+                     NOT NULL,
 
-supplier     NVARCHAR(128)
-             NOT NULL,
+        supplier     NVARCHAR(128)
+                     NOT NULL,
 
-price        MONEY
-             NOT NULL,
+        price        MONEY
+                     NOT NULL,
 
-promotion    NVARCHAR(30),
+        promotion    NVARCHAR(30),
 
-CONSTRAINT supplier_foreign FOREIGN KEY (supplier)
-    REFERENCES Suppliers(name)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        CONSTRAINT supplier_foreign FOREIGN KEY (supplier)
+            REFERENCES Suppliers(name)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
 
-CONSTRAINT producer_foreign FOREIGN KEY (producer)
-    REFERENCES Producers(name)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
+        CONSTRAINT producer_foreign FOREIGN KEY (producer)
+            REFERENCES Producers(name)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        );
+    END
 
-CREATE TABLE Workers
-(
-worker_id    INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Workers')
+    BEGIN
+        CREATE TABLE Workers
+        (
+        worker_id    INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-fullname     NVARCHAR(128)
-             NOT NULL
-             UNIQUE,
+        fullname     NVARCHAR(128)
+                     NOT NULL
+                     UNIQUE,
 
-salary       MONEY,
+        salary       MONEY,
 
-job          NVARCHAR(32),
+        job          NVARCHAR(32),
 
-address      NVARCHAR(128)
-             NOT NULL,
+        address      NVARCHAR(128)
+                     NOT NULL,
 
-passport_number CHAR(10)
-             NOT NULL
-             UNIQUE,
+        passport_number CHAR(10)
+                     NOT NULL
+                     UNIQUE,
 
-telephone    VARCHAR(16)
-             UNIQUE,
+        telephone    VARCHAR(16)
+                     UNIQUE,
 
-email        VARCHAR(128)
-             NOT NULL
-             UNIQUE
-);
+        email        VARCHAR(128)
+                     NOT NULL
+                     UNIQUE
+        );
+    END
 
-CREATE TABLE Purchases
-(
-purchase_id  INT
-             IDENTITY
-             PRIMARY KEY,
+IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Purchases')
+    BEGIN
+        CREATE TABLE Purchases
+        (
+        purchase_id  INT
+                     IDENTITY
+                     PRIMARY KEY,
 
-product_id   INT
-             NOT NULL,
+        product_id   INT
+                     NOT NULL,
 
-worker_id    INT,
+        worker_id    INT,
 
-customer_id  INT
-             NOT NULL,
+        customer_id  INT
+                     NOT NULL,
 
-quantity     SMALLINT
-             NOT NULL,
+        quantity     SMALLINT
+                     NOT NULL,
 
-date         DATETIME
-             NOT NULL
-             DEFAULT GETDATE(),
+        date         DATETIME
+                     NOT NULL
+                     DEFAULT GETDATE(),
 
-total_cost   MONEY
-             NOT NULL,
+        total_cost   MONEY
+                     NOT NULL,
 
-CONSTRAINT product_foreign FOREIGN KEY (product_id)
-    REFERENCES Products(product_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+        CONSTRAINT product_foreign FOREIGN KEY (product_id)
+            REFERENCES Products(product_id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
 
-CONSTRAINT worker_foreign FOREIGN KEY (worker_id)
-    REFERENCES Workers(worker_id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE,
+        CONSTRAINT worker_foreign FOREIGN KEY (worker_id)
+            REFERENCES Workers(worker_id)
+                ON DELETE SET NULL
+                ON UPDATE CASCADE,
 
-CONSTRAINT customer_foreign FOREIGN KEY (customer_id)
-    REFERENCES Customers(customer_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-)
+        CONSTRAINT customer_foreign FOREIGN KEY (customer_id)
+            REFERENCES Customers(customer_id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        )
+    END
