2883fd33aee4182e10937d7195c3ed2935bb45cf
lpshkn <kofffsh@gmail.com>
2020-08-18 11:01:03 +0300

    Add fixes into the scripts
    Fix inserting card_id values in the Customers table

--- database_scripts/create_tables.sql
+++ database_scripts/create_tables.sql
@@ -219,14 +219,14 @@ IF NOT EXISTS(SELECT * FROM shopdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 
 EXEC sys.sp_cdc_enable_table
 @source_schema = N'dbo',
 @source_name = N'Customers',
-@role_name = N'CDCRole',
+@role_name = NULL,
 @supports_net_changes = 1
 GO
 
 EXEC sys.sp_cdc_enable_table
 @source_schema = N'dbo',
 @source_name = N'Producers',
-@role_name = N'CDCRole',
+@role_name = NULL,
 @supports_net_changes = 1
 GO
 
